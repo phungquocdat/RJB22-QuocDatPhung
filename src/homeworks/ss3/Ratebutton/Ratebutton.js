e838aa31aa88e43fcb6c271926b9ac767de367d4
unknown <ddatj1997@gmail.com>
2022-07-11 21:14:09 +0700

    buoi 4

---
+++ src/homeworks/ss3/Ratebutton/Ratebutton.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react'
+
+export default class Ratebutton extends Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            star: 0
+        }
+    };
+    rateStar = (star) => {
+        let content=[]
+    }
+  render() {
+    return (
+      <div>Ratebutton</div>
+    )
+  }
+}
