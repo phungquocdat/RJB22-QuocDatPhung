cb2f0238fb01926de755a3a21ded56a08df7427f
unknown <ddatj1997@gmail.com>
2022-07-13 13:58:29 +0700

    b4

--- src/homeworks/ss3/Ratebutton/Ratebutton.js
+++ src/homeworks/ss3/Ratebutton/Ratebutton.js
@@ -1,18 +1,35 @@
 import React, { Component } from 'react'
+import { AiFillStar, AiOutlineStar } from 'react-icons/ai';
+const MAX_STAR = 5
 
 export default class Ratebutton extends Component {
-    constructor(props){
+    constructor(props) {
         super(props);
         this.state = {
-            star: 0
+            star : 0
         }
-    };
-    rateStar = (star) => {
-        let content=[]
     }
+    setStar = (abc) => {
+        this.setState({
+          star: abc,
+        });
+    }   
+    renderStar(star) {
+        let result = []
+        for (let index=0; index < star; index++) {
+            result.push(<span onClick={this.setStar.bind(this.setState, index + 1)}><AiFillStar style={{color:'yellow', fontSize:'50px'}}/></span>)
+        }
+        for (let index= star; index < MAX_STAR; index++) {
+            result.push(<span onClick={this.setStar.bind(this.setState, index + 1)}><AiOutlineStar style={{fontSize:'50px'}}/></span>)
+        }
+        return result;
+    }
+
   render() {
     return (
-      <div>Ratebutton</div>
+      <div>
+        <p>{this.renderStar(this.state.star)}</p>
+      </div>
     )
   }
 }
