cd5ea33ea5f21b3defa6b88f61bfe24f1a70fea8
unknown <ddatj1997@gmail.com>
2022-07-15 15:09:21 +0700

    accordion

--- src/homeworks/ss3/AccordionItem/AccordionItem.js
+++ src/homeworks/ss3/AccordionItem/AccordionItem.js
@@ -4,9 +4,10 @@ import './Accordion.css'
 export default function AccordionItem(props) {
     const {item, index} = props;
     const [isActive, setIsActive] = useState(false);
+ 
   return (
     <div className='accordion' key={`item-${index}`} onClick={() => setIsActive(!isActive)}>
-        <div className='title'>
+        <div className={isActive ? 'active' : 'title'} >
         {item.title}  
         </div>
         {isActive && <div className='border' >{item.content}</div>}
